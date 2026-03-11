b61a2c1c5ffd5f36256cda72bc45cdfa3da454d6
huizhiLLL <3169164181@qq.com>
2026-03-11 20:00:10 +0800

    feat：增加页脚

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <script setup lang="ts">
+import { Copyright } from 'lucide-vue-next'
 import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+  <div class="site-shell">
+    <div class="site-content">
+      <RouterView />
+    </div>
+
+    <footer class="site-footer">
+      <p class="site-footer-inline">
+        <Copyright :size="15" />
+        <span>All rights reserved.</span>
+        <span class="site-footer-separator">•</span>
+        <span>By</span>
+        <a class="site-footer-link" href="https://github.com/huizhiLLL" target="_blank" rel="noreferrer">
+          huizhiLLL
+        </a>
+      </p>
+    </footer>
+  </div>
 </template>
